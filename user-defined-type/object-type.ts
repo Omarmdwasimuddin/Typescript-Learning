8abd5d75a3b343eb24e67bcb7edfd38cc28807bb
Md Wasim Uddin <mdwasimu015@gmail.com>
2026-01-18 09:24:02 +0600

    Object type

--- user-defined-type/object-type.ts
+++ user-defined-type/object-type.ts
@@ -1,11 +1,34 @@
-let user1: {userName:string, userId:number};
-user1 = {userName:"Wasim", userId:101};
-console.log(user1)
+//let user1: {userName:string, userId:number};
+//user1 = {userName:"Wasim", userId:101};
+//console.log(user1)
+
+//let user2: {userName:string, userId?:number}; //userId optional
+//user2 = {userName:"Wasim"};
+//console.log(user2)
 
 
-let users: object;
-users = {userName: "Wasim", userId: 101}
+//let users: object;
+//users = {userName: "Wasim", userId: 101}
 
-let user: object[];
-user = [{userName:"Wasim"},{userId:102},{age:29}];
-console.log(user)
+//let user: object[];
+//user = [{userName:"Wasim"},{userId:102},{age:29}];
+//console.log(user)
+
+
+
+let users: object[];
+users = [];
+
+let user01: {userName:string, userId:number};
+user01 = {userName:"Wasim", userId:101};
+users.push(user01)
+
+let user02: {userName:string, userId:number};
+user02 = {userName:"Omar", userId:102};
+users.push(user02)
+
+//console.log(users)
+for (const key in users) {
+    //console.log(users)
+    console.log(users[key])
+}
